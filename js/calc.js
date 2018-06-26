d6d1dcb911142d75165eb61df066f6c4f4681b0c
rosyjackson <rosyjackson2004@gmail.com>
2018-06-26 13:59:42 +0000

    Done.

--- js/calc.js
+++ js/calc.js
@@ -1,1 +1,22 @@
 //add your code here
+
+let number1 =$("#number1")
+let operator =$("#operation")
+let number2 =$("#number2")
+
+
+function validate(operator, number1, number2){
+  if (operator == "+" || operator == "-" ||operator == "/" ||operator == "*")
+  {
+  if (isNaN(number1) || number1 == "" || isNaN(number2) || number2 == "" ) 
+  $("#result").text("Sorry, one of those is not a valid number!")
+  ;
+  
+  } else {
+  answer(operator, number1, number2);
+    }
+  }
+  else {
+    $("#result").html("Sorry, that is not a valid operator.")
+  }
+}
